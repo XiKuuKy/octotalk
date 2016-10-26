b6405590609c3fe133b2b4743f675db77fff5988
xikuuky <amazing@programmer.net>
2016-10-26 08:37:14 +0700

    More advanced now. Octoderp style.

---
+++ funny.md
@@ -0,0 +1,34 @@
+    >> Hello?
+    I hate you
+    >> Why? :(
+    OK.
+    >> What? ~-~
+    I know right?
+    >> Screw you, derpface meany jerk
+    I hate you,
+    >> WHHHHYYYY????
+    I know right?
+    >> WHAT!!!!!????
+    Derp.
+    >> DERP IS ALL YOU ARE SAYING? I AM SO FREAKING PISSED AT YOU! YOU ARE SO MEAN! I AM GONNA KILL YOU!
+    OK.
+    >> Why you do dies?
+    Let's get you
+    >> ARE YOU GONNA RAPE ME???
+    Derp.
+    >> OMGGG
+    I hate you
+    >> OMGGGG
+    I know.
+    >> YOU ARE SO MEAN
+    OK.
+    >> IM GONNA KILL MYSELF
+    Derp.
+    >> WHY!!
+    OK.
+    >> ...
+    Let's get drinks
+    >> YOU HURT MY FEELING HECK NO!
+    Derp.
+    >> DERP YOURSELF
+    Bai!
--- octotalk.rb
+++ octotalk.rb
@@ -1,13 +1,58 @@
+#!/usr/bin/env ruby
+
 while 1 do
     print ">> "
-    gets
-    chance = 1 + rand(100)
+    te=gets
+    chance = 1 + rand(250)
     if chance < 21 then
       puts "I know."
     elsif 51 > chance && chance> 20 then
       puts "OK."
-    elsif 80 > chance && chance > 50 then
+    elsif 81 > chance && chance > 50 then
       puts "I know right?"
+    elsif 101 > chance && chance > 80 then
+      word = te.split(" ")[1]
+      if word == "I" or word == "i" then
+        word = "me"
+      elsif word == "we" then
+        word = "us"
+      elsif word == "get" then
+        word = te.split(" ")[2]
+      elsif word == nil
+        word = "myself"
+      end
+      puts "I absolutely love " + word
+    elsif 121 > chance and chance > 100 then
+      puts "http://explosm.net/rcg - Love this comic!"
+    elsif 141 > chance and chance > 120 then
+      puts "http://explosm.net/comics/random - Love this comic!"
+    elsif 161 > chance and chance > 140 then
+      puts "Derp."
+    elsif 181 > chance and chance > 160 then
+      word = te.split(" ")[1]
+      if word == "I" or word == "i" then
+        word = "me"
+      elsif word == "we" then
+        word = "us"
+      elsif word == "get" then
+        word = te.split(" ")[2]
+      elsif word == nil
+        word = "you"
+      end
+
+      puts "I hate " + word
+    elsif 201 > chance and chance > 180 then
+      word = te.split(" ")[1]
+      if word == "I" or word == "i" then
+        word = "me"
+      elsif word == "we" then
+        word = "us"
+      elsif word == "get" then
+        word = te.split(" ")[2]
+      elsif word == nil
+        word = "drinks"
+      end
+      puts "Let's get " + word
     else
       puts "Bai!"
       abort
